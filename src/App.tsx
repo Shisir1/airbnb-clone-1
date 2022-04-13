20d2b6d8da736b69a9d5b560d8269fcf4602c6c3
ShisirCoder <55004953+Shisir1@users.noreply.github.com>
2022-04-13 15:57:53 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-<<<<<<< HEAD
-import { BrowserRouter, Routes } from 'react-router-dom';
-
-=======
 import LocationDetailPage from './pages/LocationDetailPage';
-
-/*
-  / -> index page -> HomePage
-  /hotel-detail -> LocationDetailPage
-*/
->>>>>>> 1283fe7969a6f2df79f48100f2aa93bfef2b66bc
 
 function App() {
   return (
